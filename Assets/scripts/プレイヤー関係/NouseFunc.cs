21c2c14ef22e150b4260a44484649d36f21c9a78
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2022-06-13 20:25:21 +0900

    最終変更
    06/13end
    PlayerControllerクラスで、現在使っていない銃関係の関数をNouseFuncクラスにまとめて分離

---
+++ Assets/scripts/プレイヤー関係/NouseFunc.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NouseFunc : PlayerController
+{
+    public List<Gun> guns = new List<Gun>();//����̊i�[�z��
+    private Camera cam;//�J����
+    private int selectedGun = 0;//�I�𒆂̕���Ǘ��p���l
+    private float shotTimer;//�ˌ��Ԋu
+    UIManager uIManager;
+
+    private void Start()
+    {
+        uIManager.SettingBulletsText(ammoClip[selectedGun], ammunition[selectedGun]);
+    }
+
+    /// <summary>
+    /// �E�N���b�N�Ŕ`������
+    /// </summary>
+    public void Aim()
+    {
+        //  �}�E�X�E�{�^�������Ă���Ƃ�
+        if (Input.GetMouseButton(1))
+        {
+            //fieldOfView�R���|�[�l���g�̒l��ύX(�J�n�n�_�A�ړI�n�_�A�⊮���l)�@�@�J�n�n�_����ړI�n�_�܂ŕ⊮���l�̊����ŏ��X�ɋ߂Â���
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, guns[selectedGun].adsZoom, guns[selectedGun].adsSpeed * Time.deltaTime);
+        }
+        else
+        {   //60�͏����ݒ萔�l
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 60f, guns[selectedGun].adsSpeed * Time.deltaTime);
+        }
+    }
+
+
+
+    /// <summary>
+    /// ���N���b�N�̌��m
+    /// </summary>
+    public void Fire()
+    {
+
+        if (Input.GetMouseButton(0) && ammoClip[selectedGun] > 0 && Time.time > shotTimer)
+        {
+            FiringBullet();
+        }
+
+    }
+
+    /// <summary>
+    /// �e�ۂ̔���
+    /// </summary>
+    private void FiringBullet()
+    {
+        //�I�𒆂̏e�̒e�򌸂炷
+        ammoClip[selectedGun]--;
+
+        //Ray(����)���J�����̒�������ɐݒ�
+        Ray ray = cam.ViewportPointToRay(new Vector2(.5f, .5f));//�J�����̒��S�����̐��l
+
+
+        //���C���΂��i�J�n�n�_�ƕ����A���������R���C�_�[�̏��i�[�j
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            //Debug.Log("���������I�u�W�F�N�g��" + hit.collider.gameObject.name);
+
+            //�e���G�t�F�N�g (hit.point�̓R���C�_�[�Ƀq�b�g�����ʒu)�Fhit.point + (hit.normal * .002f)�͂�����Ȃ��悤�ɏ�����ɂ��Ă���
+            //hit normal�͓��������I�u�W�F�N�g�ɑ΂��Ē��p�̕������Ԃ����
+            //LookRotation�͎w�肵�������ɉ�
+            GameObject bulletImpactObject = Instantiate(guns[selectedGun].bulletImpact, hit.point + (hit.normal * .002f), Quaternion.LookRotation(hit.normal, Vector3.up));
+
+            //���Ԍo�߂ŏ�����悤�ɂ���
+            Destroy(bulletImpactObject, 10f);
+        }
+
+        //�ˌ��Ԋu��ݒ�
+        shotTimer = Time.time + guns[selectedGun].shootInterval;
+
+
+    }
+
+
+    /// <summary>
+    /// �����[�h
+    /// </summary>
+    private void Reload()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //�����[�h�ŕ�[����e�����擾����
+            int amountNeed = maxAmmoClip[selectedGun] - ammoClip[selectedGun];
+
+            //�K�v�Ȓe��ʂƏ����e��ʂ��r
+            int ammoAvailable = amountNeed < ammunition[selectedGun] ? amountNeed : ammunition[selectedGun];
+
+            //�e�򂪖��^���̎��̓����[�h�ł��Ȃ�&�e����������Ă���Ƃ�
+            if (amountNeed != 0 && ammunition[selectedGun] != 0)
+            {
+                //�����e�򂩂烊���[�h����e�򕪂�����
+                ammunition[selectedGun] -= ammoAvailable;
+                //�e�ɑ��U����
+                ammoClip[selectedGun] += ammoAvailable;
+            }
+        }
+    }
+
+    public void SwitchingGuns()
+    {
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+        {
+            selectedGun++;//�����e���Ǘ����鐔�l�𑝂₷
+
+            //���X�g���傫�����l�ɂȂ��Ă��Ȃ����m�F
+            if (selectedGun >= guns.Count)
+            {
+                selectedGun = 0;//���X�g���傫�Ȑ��l�ɂȂ�΂O�ɖ߂�
+            }
+            switchGun();
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+        {
+            selectedGun--;//�����e���Ǘ����鐔�l�����炷
+
+
+            if (selectedGun < 0)
+            {
+                selectedGun = guns.Count - 1;//0��菬������΃��X�g�̍ő吔�|�P�̐��l�ɐݒ肷��
+            }
+
+            //���ۂɕ����؂�ւ���֐�
+            switchGun();
+        }
+
+        //���l�L�[�̓��͌��m�ŕ����؂�ւ���
+        for (int i = 0; i < guns.Count; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString()))//���[�v�̐��l�{�P�����ĕ�����ɕϊ��B���̌�A�����ꂽ������
+            {
+                selectedGun = i;//�e���������l��ݒ�
+
+                //���ۂɕ����؂�ւ���֐�
+                switchGun();
+
+            }
+        }
+    }
+
+    void switchGun()
+    {
+        foreach (Gun gun in guns)//���X�g�����[�v����
+        {
+            gun.gameObject.SetActive(false);//�e���\��
+        }
+
+        guns[selectedGun].gameObject.SetActive(true);//�I�𒆂̏e�̂ݕ\��
+    }
+
+}
--- Assets/scripts/プレイヤー関係/PlayerController.cs
+++ Assets/scripts/プレイヤー関係/PlayerController.cs
@@ -9,36 +9,22 @@ public class PlayerController : MonoBehaviourPunCallbacks
     private Vector2 mouseInput;//���[�U�[�̃}�E�X���͂��i�[
     private float verticalMouseInput;//y���̉�]���i�[�@��]�𐧌�����������
     private Camera cam;//�J����
-
-
     Animator animator;
     [SerializeField] private Rigidbody _rigidbody;
-
-
 
     private Vector3 moveDir;//�v���C���[�̓��͂��i�[�i�ړ��j
     private Vector3 movement;//�i�ޕ������i�[����ϐ�
     private float activeMoveSpeed = 4;//���ۂ̈ړ����x
-
-
 
     public Vector3 jumpForce = new Vector3(0, 6, 0);//�W�����v�� 
     public Transform groundCheckPoint;//�n�ʂɌ����ă��C���΂��I�u�W�F�N�g 
     public LayerMask groundLayers;//�n�ʂ��ƔF�����郌�C���[ 
     Rigidbody rb;//
 
-
     public float stopSpeed = 0 ,walkSpeed = 4, runSpeed = 8;//�����̑��x�A����̑��x
-
 
     private bool cursorLock = true;//�J�[�\���̕\��/��\�� 
 
-
-    public List<Gun> guns = new List<Gun>();//����̊i�[�z��
-    private int selectedGun = 0;//�I�𒆂̕���Ǘ��p���l
-
-
-    private float shotTimer;//�ˌ��Ԋu
     [Tooltip("�����e��")]
     public int[] ammunition;
     [Tooltip("�ō������e��")]
@@ -58,6 +44,9 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
     Dictionary<AnimationClip, float> Motion_Damage;
 
+    private readonly string[] attackAnimationNames = { "Female Sword Attack 1", "Female Sword Attack 2", "Female Sword Attack 3" };
+    private readonly float[] damages = { 10, 15, 20 };
+
     private void Awake()
     {
         //�^�O����UIManager��T��
@@ -68,8 +57,6 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
         animator = GetComponent<Animator>();
 
-        string[] attackAnimationNames = { "Female Sword Attack 1" , "Female Sword Attack 2", "Female Sword Attack 3" };
-        float[] damages = { 10, 15, 20 };
 
         Dictionary<string, float> a = new Dictionary<string, float>();
         for (int i = 0; i < damages.Length; i++)
@@ -77,17 +64,14 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
         Motion_Damage = new Dictionary<AnimationClip, float>();
 
-        var animation_clips = animator.runtimeAnimatorController.animationClips;
-        var animation_names = new List<string>();
-
-        foreach(var e in animator.runtimeAnimatorController.animationClips)
+        foreach(var animClip in animator.runtimeAnimatorController.animationClips)
         {
-            foreach(var n in attackAnimationNames)
+            foreach(var animationName in attackAnimationNames)
             {
-                if (e.ToString().Contains(n))
+                if (animClip.ToString().Contains(animationName))
                 {
-                    Debug.Log(e.ToString() + "はあってあました");
-                    Motion_Damage.Add(e, a[n]);
+                    Debug.Log(animClip.ToString() + "はあってあました");
+                    Motion_Damage.Add(animClip, a[animationName]);
                 }
             }
         }
@@ -110,7 +94,6 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
 
         //�e��e�L�X�g�X�V
-        uIManager.SettingBulletsText(ammoClip[selectedGun], ammunition[selectedGun]);
 
         this.animator = GetComponent<Animator>();
 
@@ -149,17 +132,6 @@ public class PlayerController : MonoBehaviourPunCallbacks
             Jump();
         }
 
-        //�e�̐؂�ւ�
-        SwitchingGuns();
-
-        //�`������
-        Aim();
-
-        //�ˌ��֐�
-        Fire();
-
-        //�����[�h�֐�
-        Reload();
 
         //�J�[�\����\��
         UpdateCursorLock();
@@ -191,7 +163,6 @@ public class PlayerController : MonoBehaviourPunCallbacks
         }
 
         //�e��e�L�X�g�X�V
-        uIManager.SettingBulletsText(ammoClip[selectedGun], ammunition[selectedGun]);
     }
 
     /// <summary>
@@ -326,155 +297,10 @@ public class PlayerController : MonoBehaviourPunCallbacks
     /// <summary>
     /// �e�̐؂�ւ��L�[���͂����m����
     /// </summary>
-    public void SwitchingGuns()
-    {
-        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
-        {
-            selectedGun++;//�����e���Ǘ����鐔�l�𑝂₷
-
-            //���X�g���傫�����l�ɂȂ��Ă��Ȃ����m�F
-            if (selectedGun >= guns.Count)
-            {
-                selectedGun = 0;//���X�g���傫�Ȑ��l�ɂȂ�΂O�ɖ߂�
-            }
-            switchGun();
-        }
-        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
-        {
-            selectedGun--;//�����e���Ǘ����鐔�l�����炷
-
-
-            if (selectedGun < 0)
-            {
-                selectedGun = guns.Count - 1;//0��菬������΃��X�g�̍ő吔�|�P�̐��l�ɐݒ肷��
-            }
-
-            //���ۂɕ����؂�ւ���֐�
-            switchGun();
-        }
-
-        //���l�L�[�̓��͌��m�ŕ����؂�ւ���
-        for (int i = 0; i < guns.Count; i++)
-        {
-            if (Input.GetKeyDown((i + 1).ToString()))//���[�v�̐��l�{�P�����ĕ�����ɕϊ��B���̌�A�����ꂽ������
-            {
-                selectedGun = i;//�e���������l��ݒ�
-
-                //���ۂɕ����؂�ւ���֐�
-                switchGun();
-
-            }
-        }
-    }
 
     /// <summary>
     /// �e�̐؂�ւ�
     /// </summary>
-    void switchGun()
-    {
-        foreach (Gun gun in guns)//���X�g�����[�v����
-        {
-            gun.gameObject.SetActive(false);//�e���\��
-        }
-
-        guns[selectedGun].gameObject.SetActive(true);//�I�𒆂̏e�̂ݕ\��
-    }
-
-
-
-    /// <summary>
-    /// �E�N���b�N�Ŕ`������
-    /// </summary>
-    public void Aim()
-    {
-        //  �}�E�X�E�{�^�������Ă���Ƃ�
-        if (Input.GetMouseButton(1))
-        {
-            //fieldOfView�R���|�[�l���g�̒l��ύX(�J�n�n�_�A�ړI�n�_�A�⊮���l)�@�@�J�n�n�_����ړI�n�_�܂ŕ⊮���l�̊����ŏ��X�ɋ߂Â���
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, guns[selectedGun].adsZoom, guns[selectedGun].adsSpeed * Time.deltaTime);
-        }
-        else
-        {   //60�͏����ݒ萔�l
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 60f, guns[selectedGun].adsSpeed * Time.deltaTime);
-        }
-    }
-
-
-
-    /// <summary>
-    /// ���N���b�N�̌��m
-    /// </summary>
-    public void Fire()
-    {
-
-        if (Input.GetMouseButton(0) && ammoClip[selectedGun] > 0 && Time.time > shotTimer)
-        {
-            FiringBullet();
-        }
-
-    }
-
-    /// <summary>
-    /// �e�ۂ̔���
-    /// </summary>
-    private void FiringBullet()
-    {
-        //�I�𒆂̏e�̒e�򌸂炷
-        ammoClip[selectedGun]--;
-
-        //Ray(����)���J�����̒�������ɐݒ�
-        Ray ray = cam.ViewportPointToRay(new Vector2(.5f, .5f));//�J�����̒��S�����̐��l
-
-
-        //���C���΂��i�J�n�n�_�ƕ����A���������R���C�_�[�̏��i�[�j
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            //Debug.Log("���������I�u�W�F�N�g��" + hit.collider.gameObject.name);
-
-            //�e���G�t�F�N�g (hit.point�̓R���C�_�[�Ƀq�b�g�����ʒu)�Fhit.point + (hit.normal * .002f)�͂�����Ȃ��悤�ɏ�����ɂ��Ă���
-            //hit normal�͓��������I�u�W�F�N�g�ɑ΂��Ē��p�̕������Ԃ����
-            //LookRotation�͎w�肵�������ɉ�
-            GameObject bulletImpactObject = Instantiate(guns[selectedGun].bulletImpact, hit.point + (hit.normal * .002f), Quaternion.LookRotation(hit.normal, Vector3.up));
-
-            //���Ԍo�߂ŏ�����悤�ɂ���
-            Destroy(bulletImpactObject, 10f);
-        }
-
-        //�ˌ��Ԋu��ݒ�
-        shotTimer = Time.time + guns[selectedGun].shootInterval;
-
-
-    }
-
-
-    /// <summary>
-    /// �����[�h
-    /// </summary>
-    private void Reload()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            //�����[�h�ŕ�[����e�����擾����
-            int amountNeed = maxAmmoClip[selectedGun] - ammoClip[selectedGun];
-
-            //�K�v�Ȓe��ʂƏ����e��ʂ��r
-            int ammoAvailable = amountNeed < ammunition[selectedGun] ? amountNeed : ammunition[selectedGun];
-
-            //�e�򂪖��^���̎��̓����[�h�ł��Ȃ�&�e����������Ă���Ƃ�
-            if (amountNeed != 0 && ammunition[selectedGun] != 0)
-            {
-                //�����e�򂩂烊���[�h����e�򕪂�����
-                ammunition[selectedGun] -= ammoAvailable;
-                //�e�ɑ��U����
-                ammoClip[selectedGun] += ammoAvailable;
-            }
-        }
-    }
-
-    private void Attack()
-    {
-        animator.SetBool("Attack", Input.GetMouseButton(0));
-    }
 
 
 
@@ -486,6 +312,9 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
         return damage;
     }
-
+    private void Attack()
+    {
+        animator.SetBool("Attack", Input.GetMouseButton(0));
+    }
 
 }
